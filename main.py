5af8047dd1097200f137105ae8dd0cc5cf8830cb
אביאל קדם <avielkedem2@gmail.com>
2026-02-02 18:36:12 +0200

    עריכת איש קשר

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def menu():
     print("to view contacts press 2")
     print("for search contact press 3")
     print("for delete contact press 4")
+    print("for edit contact press 5")
     print("for exit press 0")
     return input("choose ")
 
@@ -18,25 +19,31 @@ while True:
         break
 
 
-    if choice == "1":
+    elif choice == "1":
         name = input("Enter a mame ")
         phone = input("Enter a phone number ")
         a1 = AddContacts(name, phone)
         add_contacts(name, phone)
 
 
-    if choice == "2":
+    elif choice == "2":
         show_contacts("contacts.txt")
 
 
-    if choice == "3":
+    elif choice == "3":
         x = input("Enter a name to search for ")
         print(search_contacts(x))
 
 
-    if choice == "4":
+    elif choice == "4":
         x = input("Enter a name to delete for ")
         deleting_contact(x)
+
+
+    elif choice == "5":
+        name = input("Enter the mame ")
+        phone = input("Enter the new phone number ")
+        number_update(name, phone)
 
 
     else:
